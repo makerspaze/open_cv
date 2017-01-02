cb77758d0d91861c25104e0ff6fc3cadf00d03f4
makerspaze <leocherianjacob@gmail.com>
2017-01-02 18:21:58 +0530

    lane detection code

--- open_cv/test.py
+++ open_cv/test.py
@@ -1,11 +1,175 @@
 import numpy as np
+import math
 import cv2
-cap=cv2.VideoCapture("challenge.mp4")
-while(cap.isOpened()):
-	ret,frame=cap.read()
-	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	cv2.imshow("frame",gray)
-	if cv2.waitKey(1)& 0xFF == ord("q"):
-        	break
-cap.release()
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+
+def perp( a ) :
+    b = np.empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
+def seg_intersect(a1,a2, b1,b2):
+    da = a2-a1
+    db = b2-b1
+    dp = a1-b1
+    dap = perp(da)
+    denom = np.dot( dap, db)
+    num = np.dot( dap, dp )
+    return (num / denom.astype(float))*db + b1
+def movingAverage(avg, new_sample, N=20):
+    if (avg == 0):
+        return new_sample
+    avg -= avg / N;
+    avg += new_sample / N;
+    return avg;
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    global avgRight
+    avgRight = (0, 0, 0, 0)
+    global avgLeft
+    avgLeft = (0, 0, 0, 0)
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    # state variables to keep track of most dominant segment
+    largestLeftLineSize = 0
+    largestRightLineSize = 0
+    largestLeftLine = (0,0,0,0)
+    largestRightLine = (0,0,0,0)
+
+    if lines is None:
+        avgx1, avgy1, avgx2, avgy2 = avgLeft
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
+        avgx1, avgy1, avgx2, avgy2 = avgRight
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw right line
+        return
+    
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            size = math.hypot(x2 - x1, y2 - y1)
+            slope = ((y2-y1)/(x2-x1))
+            # Filter slope based on incline and
+            # find the most dominent segment based on length
+            if (slope > 0.5): #right
+                if (size > largestRightLineSize):
+                    largestRightLine = (x1, y1, x2, y2)                    
+                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+            elif (slope < -0.5): #left
+                if (size > largestLeftLineSize):
+                    largestLeftLine = (x1, y1, x2, y2)
+                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+    # Define an imaginary horizontal line in the center of the screen
+    # and at the bottom of the image, to extrapolate determined segment
+    imgHeight, imgWidth = (img.shape[0], img.shape[1])
+    upLinePoint1 = np.array( [0, int(imgHeight - (imgHeight/3))] )
+    upLinePoint2 = np.array( [int(imgWidth), int(imgHeight - (imgHeight/3))] )
+    downLinePoint1 = np.array( [0, int(imgHeight)] )
+    downLinePoint2 = np.array( [int(imgWidth), int(imgHeight)] )
+    
+    # Find the intersection of dominant lane with an imaginary horizontal line
+    # in the middle of the image and at the bottom of the image.
+    p3 = np.array( [largestLeftLine[0], largestLeftLine[1]] )
+    p4 = np.array( [largestLeftLine[2], largestLeftLine[3]] )
+    upLeftPoint = seg_intersect(upLinePoint1,upLinePoint2, p3,p4)
+    downLeftPoint = seg_intersect(downLinePoint1,downLinePoint2, p3,p4)
+    if (math.isnan(upLeftPoint[0]) or math.isnan(downLeftPoint[0])):
+        avgx1, avgy1, avgx2, avgy2 = avgLeft
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
+        avgx1, avgy1, avgx2, avgy2 = avgRight
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw right line
+        return
+    cv2.line(img, (int(upLeftPoint[0]), int(upLeftPoint[1])), (int(downLeftPoint[0]), int(downLeftPoint[1])), [0, 0, 255], 8) #draw left line
+
+    # Calculate the average position of detected left lane over multiple video frames and draw
+    
+    avgx1, avgy1, avgx2, avgy2 = avgLeft
+    avgLeft = (movingAverage(avgx1, upLeftPoint[0]), movingAverage(avgy1, upLeftPoint[1]), movingAverage(avgx2, downLeftPoint[0]), movingAverage(avgy2, downLeftPoint[1]))
+    avgx1, avgy1, avgx2, avgy2 = avgLeft
+    cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
+
+    # Find the intersection of dominant lane with an imaginary horizontal line
+    # in the middle of the image and at the bottom of the image.
+    p5 = np.array( [largestRightLine[0], largestRightLine[1]] )
+    p6 = np.array( [largestRightLine[2], largestRightLine[3]] )
+    upRightPoint = seg_intersect(upLinePoint1,upLinePoint2, p5,p6)
+    downRightPoint = seg_intersect(downLinePoint1,downLinePoint2, p5,p6)
+    if (math.isnan(upRightPoint[0]) or math.isnan(downRightPoint[0])):
+        avgx1, avgy1, avgx2, avgy2 = avgLeft
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
+        avgx1, avgy1, avgx2, avgy2 = avgRight
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw right line
+        return
+    cv2.line(img, (int(upRightPoint[0]), int(upRightPoint[1])), (int(downRightPoint[0]), int(downRightPoint[1])), [0, 0, 255], 8) #draw left line
+
+    # Calculate the average position of detected right lane over multiple video frames and draw
+    
+    avgx1, avgy1, avgx2, avgy2 = avgRight
+    avgRight = (movingAverage(avgx1, upRightPoint[0]), movingAverage(avgy1, upRightPoint[1]), movingAverage(avgx2, downRightPoint[0]), movingAverage(avgy2, downRightPoint[1]))
+    avgx1, avgy1, avgx2, avgy2 = avgRight
+    cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
+
+
+
+
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+        
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((*img.shape, 3), dtype=np.uint8)
+    #avgLeft = (0, 0, 0, 0)
+    #avgRight = (0, 0, 0, 0)
+    draw_lines(line_img, lines)
+    return line_img
+
+
+
+img=cv2.imread("lll.jpg",0)
+initial_img=cv2.imread("lll.jpg")
+height = img.shape[0]
+width = img.shape[1]
+vertices = np.array( [[
+                [3*width/4, 3*height/5],
+                [width/4, 3*height/5],
+                [40, height],
+                [width - 40, height]
+            ]], dtype=np.int32 )
+blur = cv2.GaussianBlur(img,(11,11),0)
+edges = cv2.Canny(blur,100,200)
+img=edges
+mask = np.zeros_like(edges)
+if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+else:
+        ignore_mask_color = 255
+        
+            #filling pixels inside the polygon defined by "vertices" with the fill color    
+        cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+            #returning the image only where mask pixels are nonzero
+        masked_image = cv2.bitwise_and(img, mask)
+
+
+lineMarkedImage = hough_lines(masked_image, 1, np.pi/180, 40, 30, 200)
+lmi=cv2.addWeighted(initial_img, 0.8,lineMarkedImage , 1., 0.)
+cv2.imshow('image',lmi)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
